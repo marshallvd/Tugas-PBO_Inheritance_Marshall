ca173adf69d55368b9ca8ab3d13137818dccb16d
marshallvd <96987996+marshallvd@users.noreply.github.com>
2022-04-20 19:03:28 +0800

    Add files via upload

---
+++ GraphicsCard.java
@@ -0,0 +1,8 @@
+class GraphicsCard extends Laptop {
+    String tipe = "NVIDIA® GeForce® GTX 1650 With Max-Q Design, 4GB GDDR6";
+    String cekGraphics() {
+        return "Ini berasal dari class GraphicsCard";
+    }
+}
+
+
