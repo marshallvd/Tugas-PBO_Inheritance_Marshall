c362e2fbf27f5b63243b11e117f93d0d90357f0c
marshallvd <96987996+marshallvd@users.noreply.github.com>
2022-04-20 19:02:51 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String args[]){
-        Laptop laptopMarshall = new Laptop();
+        GraphicsCard laptopMarshall = new GraphicsCard();
+
 
         System.out.println(laptopMarshall.processor);
         System.out.println(laptopMarshall.merk);
+        System.out.println(laptopMarshall.tipe);
         System.out.println(laptopMarshall.cekKomputer());
         System.out.println(laptopMarshall.cekLaptop());
+        System.out.println(laptopMarshall.cekGraphics());
     }
 }
-
